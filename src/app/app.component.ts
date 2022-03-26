c29c5ef4e13adc161629e1306479f0bf0e2af845
GuilhermeQuerion <guilhermequerion@icloud.com>
2022-03-26 13:17:22 -0300

    Bck-end finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+const urlBase = 'http://localhost:3000';
+
+interface Produto {
+  id: number;
+  nome: string;
+  preco: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'comunicacao-back-end';
+export class AppComponent implements OnInit {
+
+  produtos: Produto[] = [];
+  form = new FormGroup({
+    nome: new FormControl('', Validators.required),
+    preco: new FormControl(0, [Validators.required, Validators.min(1)])
+  })
+
+  constructor(private httpClient: HttpClient) { }
+
+  ngOnInit(): void {
+    this.httpClient.get<Produto[]>(`${urlBase}/produtos`)
+      .subscribe(dados => {
+        this.produtos = dados;
+      });
+  }
+
+  adicionar() {
+    const produto = this.form.value;
+    this.httpClient.post<Produto>(`${urlBase}/produtos`, produto)
+      .subscribe(novoProduto => {
+        this.produtos.push(novoProduto);
+      })
+  }
+
+  excluir(produto: Produto) {
+    this.httpClient.delete(`${urlBase}/produtos/${produto.id}`)
+      .subscribe(() => {
+        const index = this.produtos.indexOf(produto);
+        this.produtos.splice(index, 1);
+      })
+  }
+
 }
